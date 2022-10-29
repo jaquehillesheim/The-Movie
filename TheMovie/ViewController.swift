c303f9c020c98e75d79ed5cc12d3f59420040c22
Jaqueline Hillesheim <jaquehillesheim@hotmail.com>
2022-10-29 14:46:40 -0300

    Initial Commit

---
+++ TheMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovie
+//
+//  Created by Jaqueline Hillesheim on 29/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
